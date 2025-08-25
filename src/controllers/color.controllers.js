604160757a8ca31addceb1e2f4c7857bd2b72363
Lucasfi <lukafigueroa64@gmail.com>
2025-08-25 15:42:46 -0300

    Agregue logica para obtener los colores

--- src/controllers/color.controllers.js
+++ src/controllers/color.controllers.js
@@ -1,20 +1,31 @@
 import Color from "../models/color.js";
 
 export const test = (req, res) => {
-    res.status(200)
-    res.send('Primera prueba desde el backend')
-}
+  res.status(200);
+  res.send("Primera prueba desde el backend");
+};
 
 export const crearColor = async (req, res) => {
-    try {
-        console.log(req.body)
+  try {
+    console.log(req.body);
 
-        const nuevoColor = new Color(req.body)
-        await nuevoColor.save()
+    const nuevoColor = new Color(req.body);
+    await nuevoColor.save();
 
-        res.status(200).json({ mensjae: 'El color fue creado exitosamente' });
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ mensjae: 'Error al crear el color' });
-    }
-}
+    res.status(200).json({ mensjae: "El color fue creado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensjae: "Error al crear el color" });
+  }
+};
+
+export const LeerColores = async (req, res) => {
+  try {
+    const listaColores = await Color.find();
+
+    res.status(200).json(listaColores);
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ mensaje: 'Error al leer los Colores' })
+  }
+};
--- src/routes/color.routes.js
+++ src/routes/color.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { test, crearColor } from "../controllers/color.controllers.js";
+import { test, crearColor, LeerColores } from "../controllers/color.controllers.js";
 
 const router = new Router()
 
 router.route('/test').get(test)
-router.route('/').post(crearColor)
+router.route('/').post(crearColor).get(LeerColores);
 
 export default router
